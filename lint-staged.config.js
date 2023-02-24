b9030e36fce5a39329bbdaf108b1c5ff91a0da48
Vitaliy Potapov <vitaliy.potapov@takeda.com>
2023-02-24 11:41:00 +0400

    add gh action lint

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,11 +2,13 @@ const path = require('path');
 
 // See: https://nextjs.org/docs/basic-features/eslint#lint-staged
 const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`;
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`;
 
 module.exports = {
   // See: https://github.com/okonet/lint-staged#example-run-tsc-on-changes-to-typescript-files-but-do-not-pass-any-filename-arguments
   'src/**/*.ts?(x)': () => 'tsc -p tsconfig.json --noEmit',
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand, 'prettier --write --ignore-unknown'],
-  '!(*.{js,jsx,ts,tsx})': 'prettier --write --ignore-unknown',
+  '**/*.{js,jsx,ts,tsx}': [buildEslintCommand, 'prettier --write --ignore-unknown'],
+  '!(**/*.{js,jsx,ts,tsx})': 'prettier --write --ignore-unknown',
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,13 @@ export default function App(props: SharedAppProps) {
     <SharedApp {...props}>
       <meta name="robots" content="noindex, nofollow" />
 
-      <link rel="preload" href="/fonts/GothamSSm-Book_Web.woff2" as="font" crossOrigin="" type="font/woff2" />
+      <link
+        rel="preload"
+        href="/fonts/GothamSSm-Book_Web.woff2"
+        as="font"
+        crossOrigin=""
+        type="font/woff2"
+      />
 
       <link rel="shortcut icon" href="/favicon.ico" />
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
